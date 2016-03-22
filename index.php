0ba2839c83dea64a039848697096c877c4a8a728
kuzmiakmiroslav <kuzmiakmiroslav@gmail.com>
2016-03-22 09:53:55 +0100

    init

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ include_once ("pageHeader.php");
 
 ?>
 
-
 <header id="myCarousel" class="carousel slide">
     <!-- Indicators -->
     <ol class="carousel-indicators">
@@ -16,25 +15,22 @@ include_once ("pageHeader.php");
         <li data-target="#myCarousel" data-slide-to="2"></li>
     </ol>
 
-    <!-- Wrapper for Slides -->
+    <!-- Wrapper for slides -->
     <div class="carousel-inner">
         <div class="item active">
-            <!-- Set the first background image using inline CSS below. -->
-            <div class="fill" style="background-image:url(./images/bck.jpg)"></div>
+            <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');"></div>
             <div class="carousel-caption">
                 <h2>Caption 1</h2>
             </div>
         </div>
         <div class="item">
-            <!-- Set the second background image using inline CSS below. -->
-            <div class="fill" style="background-image: url("datasrc="./image/bck.jpg" ");"></div>
+            <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
             <div class="carousel-caption">
                 <h2>Caption 2</h2>
             </div>
         </div>
         <div class="item">
-            <!-- Set the third background image using inline CSS below. -->
-            <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Thrddddee');"></div>
+            <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
             <div class="carousel-caption">
                 <h2>Caption 3</h2>
             </div>
@@ -48,19 +44,8 @@ include_once ("pageHeader.php");
     <a class="right carousel-control" href="#myCarousel" data-slide="next">
         <span class="icon-next"></span>
     </a>
-
 </header>
-<script src="js/jquery.js"></script>
 
-<!-- Bootstrap Core JavaScript -->
-<script src="js/bootstrap.min.js"></script>
-
-<!-- Script to Activate the Carousel -->
-<script>
-    $('.carousel').carousel({
-        interval: 5000 //changes the speed
-    })
-</script>
 
 <?php
 
